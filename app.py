d5299f7a4a2a1750108e66aa58bc61793dbc55bf
Wax05 <jiseop1008@gmail.com>
2023-04-06 23:21:23 +0900

    First

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from extension import db
+from routes.admin import AdminRoute
+def Create_App():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"#
+    app.register_blueprint(AdminRoute)
+    db.init_app(app)
+
+    @app.before_first_request#Sqlite3 Sql Table Init
+    def create_database():
+        db.create_all()
+    return app
+
+if __name__ == "__main__":
+    Create_App().run(debug=True)#DEV Only
